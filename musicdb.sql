657c3ba8a0a37a4d2fd25c5f7b4573cb6f8574f7
Tamandani Nsiku <sebastiangooddays@gmail.com>
2025-12-09 11:56:34 -0800

    first perpetrate

---
+++ musicdb.sql
@@ -0,0 +1,20 @@
+create table songs(
+	id int auto_increment primary key,
+    title varchar(100),
+    artist varchar(100),
+    cover varchar(255),
+    file_url varchar(255)
+);
+
+insert into songs (title, artist, covder, file_url)
+values
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+()

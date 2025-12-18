6a8987500f630fe624eb8b78c789b6212c29ac6f
Tamandani Nsiku <sebastiangooddays@gmail.com>
2025-12-18 06:53:31 -0800

    damn

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
+
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const db = require('./config/db');
 const songRoutes = require('./routes/songs')
+
+
 
 // routes
 
@@ -9,12 +14,15 @@ const songRoutes = require('./routes/songs')
 const app =express();
 const PORT = process.env.PORT || 5000;
 
+
 //middleware
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/songs", songRoutes)
+
+app.use('/songs', express.static(path.join(__dirname, 'public/songs')));
 
 //DB test
 
